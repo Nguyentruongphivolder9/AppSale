2e34ead7fcff4c702865641773cf633d363f4cef
phi <nguyentruongphi15032003@gmail.com>
2022-06-01 16:35:56 +0700

    handle login

--- app/src/main/java/com/example/appsale/presentation/views/authentications/sign_in/SignInActivity.java
+++ app/src/main/java/com/example/appsale/presentation/views/authentications/sign_in/SignInActivity.java
@@ -1,34 +1,90 @@
 package com.example.appsale.presentation.views.authentications.sign_in;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.appsale.R;
-import com.example.appsale.presentation.views.MainActivity;
+import com.example.appsale.data.datasources.remote.AppResource;
+import com.example.appsale.data.models.User;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class SignInActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     TextInputEditText inputEdtEmail , intputEdtPassword;
-    TextView tvSignUp;
+    LinearLayout signin;
+    SignInViewModel signInViewModel;
+    LinearLayout layoutLoading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_sign_in);
 
         toolbar = findViewById(R.id.toolbarLogin);
         inputEdtEmail = findViewById(R.id.textEditEmail);
         intputEdtPassword = findViewById(R.id.textEditPassword);
-        tvSignUp = findViewById(R.id.textViewSignUp);
+        signin = findViewById(R.id.sign_in);
+        layoutLoading = findViewById(R.id.layout_loading);
 
+        initView();
+
+        event();
+    }
+
+    private void initView() {
         setStatusBar();
+
+        signInViewModel = new ViewModelProvider(this).get(SignInViewModel.class);
+    }
+
+    private void event() {
+
+        // Observer data
+        signInViewModel.getUserData().observe(this, new Observer<AppResource<User>>() {
+            @Override
+            public void onChanged(AppResource<User> userAppResource) {
+                switch (userAppResource.status){
+                    case LOADING:
+                        layoutLoading.setVisibility(View.VISIBLE);
+                        break;
+                    case SUCCESS:
+                        Toast.makeText(SignInActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                        layoutLoading.setVisibility(View.GONE);
+                        break;
+                    case ERROR:
+                        Toast.makeText(SignInActivity.this, userAppResource.message, Toast.LENGTH_SHORT).show();
+                        layoutLoading.setVisibility(View.GONE);
+                        break;
+                }
+            }
+        });
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEdtEmail.getText().toString();
+                String password = intputEdtPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(SignInActivity.this, "Người dùng chưa nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                signInViewModel.login(email, password);
+            }
+        });
     }
 
     private void setStatusBar() {
