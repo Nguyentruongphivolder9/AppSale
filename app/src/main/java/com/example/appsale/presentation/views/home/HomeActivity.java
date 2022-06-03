353e504d4c258f036c03339f27bcb513b26d9235
phi <nguyentruongphi15032003@gmail.com>
2022-06-03 22:55:48 +0700

    ui menu item

--- app/src/main/java/com/example/appsale/presentation/views/home/HomeActivity.java
+++ app/src/main/java/com/example/appsale/presentation/views/home/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,21 +15,33 @@ import android.widget.Toast;
 import com.example.appsale.R;
 import com.example.appsale.data.datasources.remote.AppResource;
 import com.example.appsale.data.models.Food;
+import com.example.appsale.presentation.adapters.FoodAdapter;
 import com.example.appsale.presentation.views.authentications.sign_up.SignUpActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     HomeViewModel viewModel;
     LinearLayout layoutLoading;
+    RecyclerView rcvFood;
+    List<Food> listFoods;
+    FoodAdapter foodAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         layoutLoading = findViewById(R.id.layout_loading);
+        rcvFood = findViewById(R.id.recyclerViewFood);
+
         viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        listFoods = new ArrayList<>();
+        foodAdapter = new FoodAdapter();
+        rcvFood.setAdapter(foodAdapter);
+        rcvFood.setHasFixedSize(true);
+
 
         viewModel.getFoods().observe(this, new Observer<AppResource<List<Food>>>() {
             @Override
@@ -39,6 +52,8 @@ public class HomeActivity extends AppCompatActivity {
                         break;
                     case SUCCESS:
                         layoutLoading.setVisibility(View.GONE);
+                        listFoods = foodAppResource.data;
+                        foodAdapter.updateListProduct(listFoods);
                         break;
                     case ERROR:
                         Toast.makeText(HomeActivity.this, foodAppResource.message, Toast.LENGTH_SHORT);
