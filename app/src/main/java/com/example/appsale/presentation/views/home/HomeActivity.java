cda3b0280754d5c356f6e3a9c29426a02bcfdb33
phi <nguyentruongphi15032003@gmail.com>
2022-06-04 21:03:28 +0700

    set menu

--- app/src/main/java/com/example/appsale/presentation/views/home/HomeActivity.java
+++ app/src/main/java/com/example/appsale/presentation/views/home/HomeActivity.java
@@ -1,15 +1,18 @@
 package com.example.appsale.presentation.views.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.appsale.R;
@@ -26,8 +29,10 @@ public class HomeActivity extends AppCompatActivity {
     HomeViewModel viewModel;
     LinearLayout layoutLoading;
     RecyclerView rcvFood;
-    List<Food> listFoods;
     FoodAdapter foodAdapter;
+    TextView tvCountCart;
+    List<Food> listCart;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +40,14 @@ public class HomeActivity extends AppCompatActivity {
 
         layoutLoading = findViewById(R.id.layout_loading);
         rcvFood = findViewById(R.id.recyclerViewFood);
+        toolbar = findViewById(R.id.toolbar_home);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white, null));
+
+        listCart = new ArrayList<>();
         viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-        listFoods = new ArrayList<>();
         foodAdapter = new FoodAdapter();
         rcvFood.setAdapter(foodAdapter);
         rcvFood.setHasFixedSize(true);
@@ -52,8 +62,7 @@ public class HomeActivity extends AppCompatActivity {
                         break;
                     case SUCCESS:
                         layoutLoading.setVisibility(View.GONE);
-                        listFoods = foodAppResource.data;
-                        foodAdapter.updateListProduct(listFoods);
+                        foodAdapter.updateListProduct(foodAppResource.data);
                         break;
                     case ERROR:
                         Toast.makeText(HomeActivity.this, foodAppResource.message, Toast.LENGTH_SHORT);
@@ -64,5 +73,36 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         viewModel.fetchFoods();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        final MenuItem menuItem = menu.findItem(R.id.action_cart);
+        View actionView = menuItem.getActionView();
+        tvCountCart = actionView.findViewById(R.id.text_cart_badge);
+
+        setupBadge();
+
+        actionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOptionsItemSelected(menuItem);
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_cart:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupBadge() {
+
     }
 }
